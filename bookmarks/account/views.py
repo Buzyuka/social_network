ddfc4f1de622a4146c50eee563713bf4d97d637d
Buzyuka <swstr_master@rambler.ru>
2023-08-03 10:49:51 +0300

    сделана форма входаэ

---
+++ bookmarks/account/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+
+# Модель аутентификации
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Аутентификация прошла успешно')
+                else:
+                    return HttpResponse('Аккаунт заблокирован')
+            else:
+                return HttpResponse('Неверный логин')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
