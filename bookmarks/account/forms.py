8279638827934c2bfc3bbbec530cb90b53ad454e
Buzyuka <swstr_master@rambler.ru>
2023-08-06 19:31:27 +0300

    Добавлена форма сброса и смены пароля

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.contrib.auth import views as auth_views
+from . import views
+
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label="Логин")
-    password = forms.CharField(widget=forms.PasswordInput, label="Пароль")
+    username = forms.CharField(label='Логин')
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -13,5 +13,17 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
+    #url-aдреса смены пароля
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    #url-адреса сброса пароля
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    #path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
 ]
