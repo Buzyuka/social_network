00d3e1e4cfab2ab1229c5b345745e7b1161f0238
Buzyuka <swstr_master@rambler.ru>
2023-08-07 14:01:50 +0300

    Добавлен новый модуль User

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from django.contrib.auth import views as auth_views
-from . import views
+from django.contrib.auth.models import User
 
 
-class LoginForm(forms.Form):
+class LoginForm(forms.ModelForm):
     username = forms.CharField(label='Логин')
-    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+
+    class Meta:
+        model: User
+        fields = ['username']
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
+from django.contrib.auth.models import User
 
 
 urlpatterns = [
@@ -10,20 +11,20 @@ urlpatterns = [
 
     # url-адреса входа и выхода
 
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #path('login/', auth_views.LoginView.as_view(), name='login'),
+    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     #url-aдреса смены пароля
-    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    #path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    #path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
     #url-адреса сброса пароля
 
-    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password-reset/complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    #path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    #path('password-reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    #path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    #path('password-reset/complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
-    #path('', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
 ]
